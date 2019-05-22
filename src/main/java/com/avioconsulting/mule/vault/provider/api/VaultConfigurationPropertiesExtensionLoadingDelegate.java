22e8d25cdef07b75623cd0a9ce979a8180403269
Adam Mead <amead@avioconsulting.com>
2019-05-22 16:36:49 -0500

    Updated property names in UI to be more readable

--- src/main/java/com/avioconsulting/mule/vault/provider/api/VaultConfigurationPropertiesExtensionLoadingDelegate.java
+++ src/main/java/com/avioconsulting/mule/vault/provider/api/VaultConfigurationPropertiesExtensionLoadingDelegate.java
@@ -7,6 +7,8 @@ import static org.mule.runtime.api.meta.ExpressionSupport.NOT_SUPPORTED;
 import org.mule.metadata.api.builder.BaseTypeBuilder;
 import org.mule.runtime.api.meta.ExpressionSupport;
 import org.mule.runtime.api.meta.model.declaration.fluent.*;
+import org.mule.runtime.api.meta.model.display.DisplayModel;
+import org.mule.runtime.api.meta.model.display.LayoutModel;
 import org.mule.runtime.extension.api.loader.ExtensionLoadingContext;
 import org.mule.runtime.extension.api.loader.ExtensionLoadingDelegate;
 
@@ -37,7 +39,9 @@ public class VaultConfigurationPropertiesExtensionLoadingDelegate implements Ext
 
     ParameterGroupDeclarer defaultParameterGroup = configurationDeclarer.onDefaultParameterGroup();
     defaultParameterGroup
-            .withRequiredParameter("vaultUrl").ofType(BaseTypeBuilder.create(JAVA).stringType().build())
+            .withRequiredParameter("vaultUrl")
+            .withDisplayModel(DisplayModel.builder().displayName("Vault URL").build())
+            .ofType(BaseTypeBuilder.create(JAVA).stringType().build())
             .withExpressionSupport(ExpressionSupport.SUPPORTED)
             .describedAs("URL for the Vault Server");
 
@@ -59,11 +63,15 @@ public class VaultConfigurationPropertiesExtensionLoadingDelegate implements Ext
             .onParameterGroup(BASIC_PARAMETER_GROUP)
             .withDslInlineRepresentation(true);
     basicParameterGroup
-            .withOptionalParameter("vaultToken").ofType(BaseTypeBuilder.create(JAVA).stringType().build())
+            .withOptionalParameter("vaultToken")
+            .withDisplayModel(DisplayModel.builder().displayName("Vault Token").build())
+            .ofType(BaseTypeBuilder.create(JAVA).stringType().build())
             .withExpressionSupport(ExpressionSupport.SUPPORTED)
             .describedAs("Vault Token with access to necessary secrets");
     basicParameterGroup
-            .withOptionalParameter("kvVersion").ofType(BaseTypeBuilder.create(JAVA).numberType().build())
+            .withOptionalParameter("kvVersion")
+            .withDisplayModel(DisplayModel.builder().displayName("KV Version").build())
+            .ofType(BaseTypeBuilder.create(JAVA).numberType().build())
             .withExpressionSupport(ExpressionSupport.SUPPORTED)
             .describedAs("KV Version Number");
 
@@ -79,15 +87,21 @@ public class VaultConfigurationPropertiesExtensionLoadingDelegate implements Ext
             .onParameterGroup(SSL_PARAMETER_GROUP)
             .withDslInlineRepresentation(true);
     sslParameterGroup
-            .withOptionalParameter("pemFile").ofType(BaseTypeBuilder.create(JAVA).stringType().build())
+            .withOptionalParameter("pemFile")
+            .withDisplayModel(DisplayModel.builder().displayName("PEM File").build())
+            .ofType(BaseTypeBuilder.create(JAVA).stringType().build())
             .withExpressionSupport(NOT_SUPPORTED)
             .describedAs("An X.509 certificate, to use when communicating with Vault over HTTPS.");
     sslParameterGroup
-            .withOptionalParameter("trustStorePath").ofType(BaseTypeBuilder.create(JAVA).stringType().build())
+            .withOptionalParameter("trustStorePath")
+            .withDisplayModel(DisplayModel.builder().displayName("Trust Store Path").build())
+            .ofType(BaseTypeBuilder.create(JAVA).stringType().build())
             .withExpressionSupport(NOT_SUPPORTED)
             .describedAs("Path to the trust store for trusted certificates");
     sslParameterGroup
-            .withOptionalParameter("verifySSL").ofType(BaseTypeBuilder.create(JAVA).booleanType().build())
+            .withOptionalParameter("verifySSL")
+            .withDisplayModel(DisplayModel.builder().displayName("Verify SSL").build())
+            .ofType(BaseTypeBuilder.create(JAVA).booleanType().build())
             .withExpressionSupport(NOT_SUPPORTED)
             .describedAs("Should Vault SSL be verified")
             .defaultingTo(Boolean.FALSE);
@@ -103,19 +117,28 @@ public class VaultConfigurationPropertiesExtensionLoadingDelegate implements Ext
             .onParameterGroup(TLS_PARAMETER_GROUP)
             .withDslInlineRepresentation(true);
     sslParameterGroup
-            .withOptionalParameter("clientPemFile").ofType(BaseTypeBuilder.create(JAVA).stringType().build())
+            .withOptionalParameter("clientPemFile")
+            .withDisplayModel(DisplayModel.builder().displayName("Client PEM File").build())
+            .ofType(BaseTypeBuilder.create(JAVA).stringType().build())
             .withExpressionSupport(NOT_SUPPORTED)
             .describedAs("An X.509 client certificate, for use with Vault's TLS Certificate auth backend.");
     sslParameterGroup
-            .withOptionalParameter("clientKeyPemFile").ofType(BaseTypeBuilder.create(JAVA).stringType().build())
+            .withOptionalParameter("clientKeyPemFile")
+            .withDisplayModel(DisplayModel.builder().displayName("Client Key PEM File").build())
+            .ofType(BaseTypeBuilder.create(JAVA).stringType().build())
             .withExpressionSupport(NOT_SUPPORTED)
             .describedAs("An RSA private key, for use with Vault's TLS Certificate auth backend.");
     sslParameterGroup
-            .withOptionalParameter("keyStorePath").ofType(BaseTypeBuilder.create(JAVA).stringType().build())
+            .withOptionalParameter("keyStorePath")
+            .withDisplayModel(DisplayModel.builder().displayName("Key Store Path").build())
+            .ofType(BaseTypeBuilder.create(JAVA).stringType().build())
             .withExpressionSupport(NOT_SUPPORTED)
             .describedAs("Path to the key store with the vault registered certificate");
     sslParameterGroup
-            .withOptionalParameter("keyStorePassword").ofType(BaseTypeBuilder.create(JAVA).stringType().build())
+            .withOptionalParameter("keyStorePassword")
+            .withDisplayModel(DisplayModel.builder().displayName("Key Store Password").build())
+            .withLayout(LayoutModel.builder().asPassword().build())
+            .ofType(BaseTypeBuilder.create(JAVA).stringType().build())
             .withExpressionSupport(NOT_SUPPORTED)
             .describedAs("Password for the key store");
   }
@@ -130,27 +153,37 @@ public class VaultConfigurationPropertiesExtensionLoadingDelegate implements Ext
             .onParameterGroup(IAM_PARAMETER_GROUP)
             .withDslInlineRepresentation(true);
     iamParameterGroup
-            .withOptionalParameter("iamAwsAuthMount").ofType(BaseTypeBuilder.create(JAVA).stringType().build())
+            .withOptionalParameter("iamAwsAuthMount")
+            .withDisplayModel(DisplayModel.builder().displayName("IAM AWS Auth Mount").build())
+            .ofType(BaseTypeBuilder.create(JAVA).stringType().build())
             .withExpressionSupport(NOT_SUPPORTED)
             .describedAs("Mount point for AWS Authentication in Vault");
     iamParameterGroup
-            .withOptionalParameter("iamVaultRole").ofType(BaseTypeBuilder.create(JAVA).stringType().build())
+            .withOptionalParameter("iamVaultRole")
+            .withDisplayModel(DisplayModel.builder().displayName("Vault Role").build())
+            .ofType(BaseTypeBuilder.create(JAVA).stringType().build())
             .withExpressionSupport(NOT_SUPPORTED)
             .describedAs("Name of the role against which the login is being attempted. If role is not specified, then the login " +
                     "endpoint looks for a role bearing the name of the AMI ID of the EC2 instance that is trying to login if " +
                     "using the ec2 auth method, or the \"friendly name\" (i.e., role name or username) of the IAM principal " +
                     "authenticated. If a matching role is not found, login fails.");
     iamParameterGroup
-            .withOptionalParameter("iamUrl").ofType(BaseTypeBuilder.create(JAVA).stringType().build())
+            .withOptionalParameter("iamUrl")
+            .withDisplayModel(DisplayModel.builder().displayName("IAM URL").build())
+            .ofType(BaseTypeBuilder.create(JAVA).stringType().build())
             .withExpressionSupport(NOT_SUPPORTED)
             .describedAs("Most likely https://sts.amazonaws.com/")
             .defaultingTo("https://sts.amazonaws.com/");
     iamParameterGroup
-            .withOptionalParameter("iamReqBody").ofType(BaseTypeBuilder.create(JAVA).stringType().build())
+            .withOptionalParameter("iamReqBody")
+            .withDisplayModel(DisplayModel.builder().displayName("IAM Request Body").build())
+            .ofType(BaseTypeBuilder.create(JAVA).stringType().build())
             .withExpressionSupport(NOT_SUPPORTED)
             .describedAs("Body of the signed request");
     iamParameterGroup
-            .withOptionalParameter("iamReqHeaders").ofType(BaseTypeBuilder.create(JAVA).stringType().build())
+            .withOptionalParameter("iamReqHeaders")
+            .withDisplayModel(DisplayModel.builder().displayName("IAM Request Headers").build())
+            .ofType(BaseTypeBuilder.create(JAVA).stringType().build())
             .withExpressionSupport(NOT_SUPPORTED)
             .describedAs("IAM Request Headers");
   }
@@ -165,27 +198,39 @@ public class VaultConfigurationPropertiesExtensionLoadingDelegate implements Ext
             .onParameterGroup(EC2_PARAMETER_GROUP)
             .withDslInlineRepresentation(true);
     ec2ParameterGroup
-            .withOptionalParameter("ec2AwsAuthMount").ofType(BaseTypeBuilder.create(JAVA).stringType().build())
+            .withOptionalParameter("ec2AwsAuthMount")
+            .withDisplayModel(DisplayModel.builder().displayName("AWS Auth Mount").build())
+            .ofType(BaseTypeBuilder.create(JAVA).stringType().build())
             .withExpressionSupport(NOT_SUPPORTED)
             .describedAs("Mount point for AWS Authentication in Vault");
     ec2ParameterGroup
-            .withOptionalParameter("ec2VaultRole").ofType(BaseTypeBuilder.create(JAVA).stringType().build())
+            .withOptionalParameter("ec2VaultRole")
+            .withDisplayModel(DisplayModel.builder().displayName("Vault Role").build())
+            .ofType(BaseTypeBuilder.create(JAVA).stringType().build())
             .withExpressionSupport(NOT_SUPPORTED)
             .describedAs("");
     ec2ParameterGroup
-            .withOptionalParameter("pkcs7").ofType(BaseTypeBuilder.create(JAVA).stringType().build())
+            .withOptionalParameter("pkcs7")
+            .withDisplayModel(DisplayModel.builder().displayName("PKCS7").build())
+            .ofType(BaseTypeBuilder.create(JAVA).stringType().build())
             .withExpressionSupport(NOT_SUPPORTED)
             .describedAs("PKCS7 signature of the identity document with all \\n characters removed.");
     ec2ParameterGroup
-            .withOptionalParameter("identityDoc").ofType(BaseTypeBuilder.create(JAVA).stringType().build())
+            .withOptionalParameter("identityDoc")
+            .withDisplayModel(DisplayModel.builder().displayName("Identity Document").build())
+            .ofType(BaseTypeBuilder.create(JAVA).stringType().build())
             .withExpressionSupport(NOT_SUPPORTED)
             .describedAs("Base64 encoded EC2 instance identity document.");
     ec2ParameterGroup
-            .withOptionalParameter("identityDocSignature").ofType(BaseTypeBuilder.create(JAVA).stringType().build())
+            .withOptionalParameter("identityDocSignature")
+            .withDisplayModel(DisplayModel.builder().displayName("Identity Document Signature").build())
+            .ofType(BaseTypeBuilder.create(JAVA).stringType().build())
             .withExpressionSupport(NOT_SUPPORTED)
             .describedAs("Base64 encoded SHA256 RSA signature of the instance identity document");
     ec2ParameterGroup
-            .withOptionalParameter("useInstanceMetadata").ofType(BaseTypeBuilder.create(JAVA).booleanType().build())
+            .withOptionalParameter("useInstanceMetadata")
+            .withDisplayModel(DisplayModel.builder().displayName("Use Instance Metadata").build())
+            .ofType(BaseTypeBuilder.create(JAVA).booleanType().build())
             .withExpressionSupport(NOT_SUPPORTED)
             .describedAs("Retrieve Instance metadata")
             .defaultingTo(Boolean.FALSE);
