76316f9eda670898274f2eeaea7d8b2c9a3bf10c
Adam Mead <amead@avioconsulting.com>
2019-12-05 11:47:55 -0600

    Extension property updates for MuleSoft certification process

--- src/main/java/com/avioconsulting/mule/vault/provider/api/VaultPropertiesProviderExtension.java
+++ src/main/java/com/avioconsulting/mule/vault/provider/api/VaultPropertiesProviderExtension.java
@@ -1,15 +1,18 @@
 package com.avioconsulting.mule.vault.provider.api;
 
 import org.mule.runtime.api.component.ComponentIdentifier;
+import org.mule.runtime.api.meta.Category;
 import org.mule.runtime.extension.api.annotation.Configurations;
 import org.mule.runtime.extension.api.annotation.Export;
 import org.mule.runtime.extension.api.annotation.Extension;
 import org.mule.runtime.extension.api.annotation.dsl.xml.Xml;
+import org.mule.runtime.extension.api.annotation.license.RequiresEnterpriseLicense;
 
 import static org.mule.runtime.api.component.ComponentIdentifier.builder;
 
 @Xml(prefix = "vault-properties-provider")
-@Extension(name = "Vault Properties Provider", vendor = "AVIO Consulting")
+@Extension(name = "Vault Properties Provider", category = Category.CERTIFIED, vendor = "AVIO Consulting")
+@RequiresEnterpriseLicense(allowEvaluationLicense = true)
 @Configurations(VaultPropertiesProviderConfiguration.class)
 @Export(classes = VaultConfigurationPropertiesProviderFactory.class,
         resources = "META-INF/services/org.mule.runtime.config.api.dsl.model.properties.ConfigurationPropertiesProviderFactory")
