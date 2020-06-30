e0f1839de5237375caeabf949a385f62e04a3089
Adam Mead <amead@avioconsulting.com>
2020-06-30 17:18:07 -0500

    Added custom exception classes in the appropriate package and switched exceptions thrown by provider

---
+++ src/main/java/com/avioconsulting/mule/vault/provider/internal/error/exception/SecretNotFoundException.java
@@ -0,0 +1,13 @@
+package com.avioconsulting.mule.vault.provider.internal.error.exception;
+
+import com.avioconsulting.mule.vault.provider.internal.error.VaultErrorType;
+import org.mule.runtime.extension.api.exception.ModuleException;
+
+public class SecretNotFoundException extends ModuleException {
+    public SecretNotFoundException(Exception cause) {
+        super(VaultErrorType.SECRET_NOT_FOUND, cause);
+    }
+    public SecretNotFoundException(String message, Throwable cause) {
+        super(message, VaultErrorType.SECRET_NOT_FOUND, cause);
+    }
+}
