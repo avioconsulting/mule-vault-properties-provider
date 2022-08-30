a80a82cd8c30b481fdece0ca912698f50920d53c
Gustavo Henrique Gomes <gustavo.gomes@avioconsulting.com>
2022-08-30 09:57:54 -0300

    Added a unit test to the fallback file implementation

---
+++ src/test/java/com/avioconsulting/mule/vault/provider/api/PropertiesProviderFallbackFile.java
@@ -0,0 +1,24 @@
+package com.avioconsulting.mule.vault.provider.api;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.mule.functional.junit4.MuleArtifactFunctionalTestCase;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+public class PropertiesProviderFallbackFile extends MuleArtifactFunctionalTestCase {
+    @Override
+    protected String getConfigFile() {
+        return "mule_config/test-mule-fallback-file-config.xml";
+    }
+    @Test
+    public void testEmptyEnvironmentVariable() throws Exception {
+            String payloadValue = ((String) flowRunner("testFlow")
+                    .run()
+                    .getMessage()
+                    .getPayload()
+                    .getValue());
+            assertThat(payloadValue, is("I-came-from-file"));
+    }
+}
