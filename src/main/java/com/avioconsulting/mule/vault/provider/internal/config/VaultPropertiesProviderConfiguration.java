dfe9ba2fb4c530e0925df13a8f147350b4cb6816
Adam Mead <amead@avioconsulting.com>
2022-09-06 22:49:08 -0500

    Organize imports

--- src/main/java/com/avioconsulting/mule/vault/provider/internal/config/VaultPropertiesProviderConfiguration.java
+++ src/main/java/com/avioconsulting/mule/vault/provider/internal/config/VaultPropertiesProviderConfiguration.java
@@ -1,8 +1,13 @@
 package com.avioconsulting.mule.vault.provider.internal.config;
 
-import com.avioconsulting.mule.vault.provider.internal.connection.provider.*;
 import org.mule.runtime.extension.api.annotation.Configuration;
 import org.mule.runtime.extension.api.annotation.connectivity.ConnectionProviders;
+
+import com.avioconsulting.mule.vault.provider.internal.connection.provider.AppRoleConnectionProvider;
+import com.avioconsulting.mule.vault.provider.internal.connection.provider.Ec2ConnectionProvider;
+import com.avioconsulting.mule.vault.provider.internal.connection.provider.IamConnectionProvider;
+import com.avioconsulting.mule.vault.provider.internal.connection.provider.TlsConnectionProvider;
+import com.avioconsulting.mule.vault.provider.internal.connection.provider.TokenConnectionProvider;
 
 @Configuration(name="config")
 @ConnectionProviders({TokenConnectionProvider.class, TlsConnectionProvider.class, IamConnectionProvider.class, Ec2ConnectionProvider.class, AppRoleConnectionProvider.class})
--- src/main/java/com/avioconsulting/mule/vault/provider/internal/connection/impl/TlsConnection.java
+++ src/main/java/com/avioconsulting/mule/vault/provider/internal/connection/impl/TlsConnection.java
@@ -1,19 +1,20 @@
 package com.avioconsulting.mule.vault.provider.internal.connection.impl;
 
-import com.avioconsulting.mule.vault.provider.api.connection.parameters.*;
-import com.bettercloud.vault.SslConfig;
-import com.bettercloud.vault.Vault;
-import com.bettercloud.vault.VaultConfig;
-import com.bettercloud.vault.VaultException;
-import org.mule.runtime.api.connection.ConnectionException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import java.io.File;
 import java.io.IOException;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
 import java.security.cert.CertificateException;
+
+import org.mule.runtime.api.connection.ConnectionException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.avioconsulting.mule.vault.provider.api.connection.parameters.EngineVersion;
+import com.avioconsulting.mule.vault.provider.api.connection.parameters.TlsContext;
+import com.bettercloud.vault.SslConfig;
+import com.bettercloud.vault.Vault;
+import com.bettercloud.vault.VaultConfig;
+import com.bettercloud.vault.VaultException;
 
 public class TlsConnection extends AbstractConnection {
 
