f5de7da89ffe7f03c59efdef76985f51ca4af42e
Gustavo Gomes <gustavo.gomes@avioconsulting.com>
2023-08-03 11:55:53 -0300

    feat: Replacing Better cloud client with vault-http-client
    Improved and Fix Munit tests

--- src/test/resources/container/config/configure.sh
+++ src/test/resources/container/config/configure.sh
@@ -4,3 +4,4 @@ export VAULT_ADDR='http://127.0.0.1:8200'
 export VAULT_TOKEN='test_token'
 
 vault kv put -mount=secret sample-secret att='Vault is working'
+vault kv put -mount=secret multi-secret att1='Vault is working to multi properties secret' att2='Vault is working to multi properties secret2'
